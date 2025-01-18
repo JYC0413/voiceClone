4273766fe4a793d05c25003a59bc73077528b155
JYC0413 <juyichen0413@foxmail.com>
2025-01-18 21:26:16 +0800

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+ICON_FOLDER = os.path.join(os.getcwd(), 'icons')
+@app.route('/icons/<path:filename>')
+def show_icon(filename):
+    return send_from_directory(ICON_FOLDER, filename)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def page2():
+    return render_template('dashboard.html')
+
+@app.route('/clone')
+def page3():
+    return render_template('clone.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
